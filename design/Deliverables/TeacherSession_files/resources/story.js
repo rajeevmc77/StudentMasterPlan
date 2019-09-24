4303e7864cce7d913b9ab9c7d74b0475aa6ce003
Rajeev <rajeevmc77@gmail.com>
2019-09-24 16:59:18 +0530

    first commit

---
+++ design/Deliverables/TeacherSession_files/resources/story.js
@@ -0,0 +1,75 @@
+var story = {
+ "pages": [
+  {
+   "image": "Login.png",
+   "image2x": "Login@2x.png",
+   "width": 817,
+   "links": [{
+    "rect": [
+     664,
+     264,
+     716,
+     287
+    ],
+    "page": 1
+   }],
+   "title": "Login",
+   "height": 505
+  },
+  {
+   "image": "TeacherHome.png",
+   "image2x": "TeacherHome@2x.png",
+   "width": 768,
+   "links": [
+    {
+     "rect": [
+      593,
+      148,
+      625,
+      180
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      637,
+      149,
+      669,
+      181
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      682,
+      150,
+      714,
+      182
+     ],
+     "page": 3
+    }
+   ],
+   "title": "TeacherHome",
+   "height": 421
+  },
+  {
+   "image": "StudentDataEdit.png",
+   "image2x": "StudentDataEdit@2x.png",
+   "width": 791,
+   "links": [],
+   "title": "StudentDataEdit",
+   "height": 500
+  },
+  {
+   "image": "Student Profile.png",
+   "image2x": "Student Profile@2x.png",
+   "width": 768,
+   "links": [],
+   "title": "Student Profile",
+   "height": 422
+  }
+ ],
+ "resolutions": [2],
+ "title": "TeacherSession",
+ "highlightLinks": false
+}
---
+++ design/TeacherSession.story
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<story:Storyboard xmlns:story="http://wireframesketcher.com/1.0/model/story">
+  <panels id="gPdInWo1qdsLdEmSVoGDJClZp30=" x="975" y="75">
+    <screen href="Login.screen#/"/>
+  </panels>
+  <panels id="rJmZxbtXGzKzMrEptlHRQfxt06s=" x="75" y="75">
+    <screen href="TeacherHome.screen#/"/>
+  </panels>
+  <panels id="u4I7O8dsgKjLZt02quqnZ696uds=" x="375" y="75">
+    <screen href="StudentDataEdit.screen#/"/>
+  </panels>
+  <panels id="bf1Ibkfky1RCrwJLGPPRVhSUnGQ=" x="675" y="75">
+    <screen href="Student%20Profile.screen#/"/>
+  </panels>
+</story:Storyboard>
